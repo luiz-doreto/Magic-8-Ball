9f20e03cee0464192ae9ca31b74fd6e3e916900e
Luiz Doreto <luiz.doreto@icloud.com>
2020-11-02 13:34:35 -0300

    Creating the interface and randomizing the ball answer.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet var ballView: UIImageView!
+    
+    let ballArray = [#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball4")]
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        ballView.image = ballArray.randomElement()
+    }
+    
 
 }
 
